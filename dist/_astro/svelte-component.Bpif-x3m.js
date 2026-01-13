e6b69f8224cd43ba99715480411e5d8606ecccbf
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-01-13 22:52:56 +0100

    Added baseurl

--- dist/_astro/svelte-component.Bpif-x3m.js
+++
@@ -1,1 +0,0 @@
-import{j as s,k as c,l as m,E as i}from"./utils.BIfnLaxk.js";import{B as p}from"./branches.B10Xz6Ex.js";function l(n,r,o){s&&c();var e=new p(n);m(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},i)}export{l as c};
