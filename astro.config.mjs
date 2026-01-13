2db8873ee156df91e4235b0384e404d8d71bfbef
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-01-13 23:02:45 +0100

    Added better prod build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import svelte from '@astrojs/svelte';
 import tailwindcss from "@tailwindcss/vite";
 import Icons from 'unplugin-icons/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://sba-interactive.github.io',
+    base: isProd ? '/sba-interactive' : undefined,
+    trailingSlash: 'always',
     output: 'static',
     integrations: [svelte()],
     vite: {
