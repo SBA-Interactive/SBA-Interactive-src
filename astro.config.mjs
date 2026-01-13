79ff04d0f3c9789bef8140bfae7b6a674731e05e
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-01-13 23:10:27 +0100

    Trying to make it work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,17 @@ import svelte from '@astrojs/svelte';
 import tailwindcss from "@tailwindcss/vite";
 import Icons from 'unplugin-icons/vite';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sba-interactive.github.io',
-    base: isProd ? '/sba-interactive' : undefined,
+    base: '/sba-interactive',
     trailingSlash: 'always',
     output: 'static',
     integrations: [svelte()],
     vite: {
-        plugins: [tailwindcss(), Icons({ compiler: 'svelte', autoInstall: true })]
+        plugins: [
+            tailwindcss(),
+            Icons({ compiler: 'svelte', autoInstall: true })
+        ]
     }
 });
